cd6a6ae53a6af0e14fd796be5442fa3da943199d
NathanCisz <NathanCisz05@gmail.com>
2024-04-26 21:11:48 -0300

    Adição dos botões para ir para outras abas, assim como uma forma rudimentas das outras abas.

--- app/src/main/java/com/example/projetointegrado2/ListaClientes.java
+++ app/src/main/java/com/example/projetointegrado2/ListaClientes.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ListaServicos extends AppCompatActivity {
+public class ListaClientes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.lista_servicos);
-        
+        setContentView(R.layout.lista_clientes);
+
     }
 }
